530bfd79967290a33bab36315359a981dfcb9270
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-15 09:30:38 +0500

    adding ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -7,12 +8,20 @@ export class RecipeService {
       new Recipe(
         'A Test Recipe',
         'This is simply a test',
-        'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
+        'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
+        ]
       ),
       new Recipe(
         'Another Test Recipe',
         'This is simply a test',
-        'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
+        'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
+        ]
       ),
   ];
 
